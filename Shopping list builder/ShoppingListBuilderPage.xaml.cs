02477944533e7a5d3f702fc9aeb91f3e5e992052
AudreyL11 <alndstrm1@gmail.com>
2024-04-05 22:16:18 -0700

    let me push dang it

--- Shopping list builder/ShoppingListBuilderPage.xaml.cs
+++ Shopping list builder/ShoppingListBuilderPage.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using Shopping_list_builder.Classes;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.Pkcs;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -33,10 +35,57 @@ namespace Shopping_list_builder
                 new Recipe("Cookies", "Yummy cookies."),
                 new Recipe("Spaghetti", "Mom's old fashioned spaghetti."),
                 new Recipe("Mac and Cheese", "Cheesiest macaroni in the world."),
-                new Recipe("Boglogna Sandwich", "Plain, but delicious."),
-                new Recipe("Kung Pao Chicken", "American Chinese Cuisine at its finest."),
-                new Recipe("Pork Fried Rice", "A staple.") 
+                //new Recipe("Boglogna Sandwich", "Plain, but delicious."),
+                //new Recipe("Kung Pao Chicken", "American Chinese Cuisine at its finest."),
+                //new Recipe("Pork Fried Rice", "A staple.") 
             };
+
+            //Adding those recipe ingredients
+            Recipe temp;
+
+            //Cookies https://www.sweetestmenu.com/chewy-snickerdoodle-cinnamon-cookies/
+            temp = recipes[0];
+            //using grams for weight
+            temp.addItem("flour", 210);
+            //by stick
+            temp.addItem("butter", 1);
+            temp.addItem("granulated sugar", 150);
+            temp.addItem("egg", 1);
+            //Dont worry about little stuff
+            temp.addItem("baking soda", -1);
+            temp.addItem("cream of tartar", -1);
+            temp.addItem("salt", -1);
+            recipes[0] = temp;
+
+            //Spaghetti https://www.food.com/recipe/jo-mamas-world-famous-spaghetti-22782
+            temp = recipes[1];
+            temp.addItem("italian sausage", 907.19);
+            temp.addItem("onion", 1);
+            temp.addItem("garlic clove", 4);
+            temp.addItem("can diced tomatoes", 1);
+            temp.addItem("can tomato paste", 2);
+            temp.addItem("can tomato sauce", 2);
+            temp.addItem("basil", -1);
+            temp.addItem("black pepper", -1);
+            temp.addItem("red wine", 1);
+            temp.addItem("spaghetti", 453.6);
+            temp.addItem("parmesan", - 1);
+            recipes[1] = temp;
+
+            //Mac and Cheese
+            temp = recipes[2];
+            temp.addItem("elbow pasta", 453.6);
+            temp.addItem("butter", 1);
+            temp.addItem("flour", 60);
+            //gallons for liquids
+            temp.addItem("whole milk", .1);
+            temp.addItem("half and half", 1.6);
+            temp.addItem("cheddar", 464);
+            temp.addItem("gruyere", 226.8);
+            temp.addItem("salt", - 1);
+            temp.addItem("black pepper", -1);
+            temp.addItem("paprika", -1);
+
 
             foreach (Recipe recipe in recipes)
             {
@@ -64,6 +113,7 @@ namespace Shopping_list_builder
             // select an item in ItemsInRecipe, add that item to Shopping List
 
             ItemsInRecipeList.Items.Add("Ingredient");
+
         }
 
         private void AddAll_Button(object sender, RoutedEventArgs e)
@@ -71,9 +121,33 @@ namespace Shopping_list_builder
             // Adds all of the items in Items in Recipe to Shopping List
         }
 
+        private void RecipesList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ArrayList itemsToDisplay = new ArrayList();
+
+            int selected = RecipesList.SelectedIndex;
+            foreach (Item i in recipes[selected].Items)
+            {
+                String temp;
+                temp = "";
+                temp = i.ID;
+                if (i.Amount != -1)
+                {
+                    temp = temp + " " + i.Amount;
+                }
+                itemsToDisplay.Add(temp);
+            }
+            ItemsInRecipeList.ItemsSource = itemsToDisplay;
+            //Keep these for later. Can populate with just the objects. Cant figure out how to display amounts from here though :/
+            //But useful to reference.
+            //ItemsInRecipeList.DisplayMemberPath = "ID";
+            //ItemsInRecipeList.ValueMember = "Amount";
+            //ItemsInRecipeList.ItemsSource = recipes[selected].Items;
+        }
+
         // ShoppingList
         // ItemsInRecipeList
         // RecipeList
-        
+
     }
 }
