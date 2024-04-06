c2e1202d89f0ac40d9a07f1580defa306de69f60
Jack Gardner <90489100+jacknelsongardner@users.noreply.github.com>
2024-04-06 04:20:19 -0700

    Updated Shopping Algorithm to load storenames from JSON file

--- Shopping list builder/Classes/Shopping Algorithm.cs
+++ Shopping list builder/Classes/Shopping Algorithm.cs
@@ -11,6 +11,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
 
 
 namespace Shopping_list_builder.Classes
@@ -19,39 +21,109 @@ namespace Shopping_list_builder.Classes
     public class ShoppingAlgorithm
     {
 
-        public static string DoStuff()
+        public static List<Dictionary<string, double>> storesFromFile;
+        public static List<string> storeNames;
+
+        public static void WriteJSONBusinesses()
         {
-            List<Dictionary<string, double>> stores = new List<Dictionary<string, double>> { new Dictionary<string, double> { { "apple", 2.50 }, { "banana", 1.20 }, { "orange", 1.80 } }, new Dictionary<string, double> { { "apple", 2.20 }, { "banana", 1.10 }, { "orange", 2.00 } }, new Dictionary<string, double> { { "apple", 2.00 }, { "banana", 1.30 }, { "orange", 1.90 } } };
+                List<Dictionary<string, double>> stores = new List<Dictionary<string, double>>
+            {
+                new Dictionary<string, double> { { "apple", 2.50 }, { "banana", 1.20 }, { "orange", 1.80 } },
+                new Dictionary<string, double> { { "apple", 2.20 }, { "banana", 1.10 }, { "orange", 2.00 } },
+                new Dictionary<string, double> { { "apple", 2.00 }, { "banana", 1.30 }, { "orange", 1.90 } }
+            };
+
+            List<string> sNames = new List<string> { "Walmart 433 Bickford Avenu", "Target 3245 Likeland Square", "QFC Broadway 91st" };
+
+
+            // Write the list of dictionaries to JSON file
+            WriteToJsonFile("storesStats.json", stores);
+            WriteToJsonFile("storesNames.json", sNames);
+        }
+
+        public static void ReadJSONBusinesses()
+        {
+            storesFromFile = ReadFromJsonFile<List<Dictionary<string, double>>>("storesStats.json");
+            storeNames = ReadFromJsonFile<List<string>>("storesNames.json");
+        }
+
+        static T ReadFromJsonFile<T>(string fileName)
+        {
+            T data;
+            if (File.Exists(fileName))
+            {
+                string jsonString = File.ReadAllText(fileName);
+                data = JsonSerializer.Deserialize<T>(jsonString);
+                Console.WriteLine($"Data has been read from {fileName}.");
+            }
+            else
+            {
+                Console.WriteLine($"File {fileName} not found. Creating a new file.");
+                data = default(T);
+                WriteToJsonFile(fileName, data);
+            }
+            return data;
+        }
+
+        // Function to write a list of dictionaries to a JSON file
+        static void WriteToJsonFile<T>(string fileName, T data)
+        {
+            string jsonString = JsonSerializer.Serialize(data, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText(fileName, jsonString);
+            Console.WriteLine($"Data has been written to {fileName}.");
+        }
+
+
+
+        public static string DoStuff()//List<Item> items, int maxStores, double maxDistance)
+        {
+            WriteJSONBusinesses();
+            ReadJSONBusinesses();
+
+            List<Dictionary<string, double>> stores = storesFromFile;
 
             // List of store names
-            List<string> storeNames = new List<string> { "Store A", "Store B", "Store C" };
+            
+            // List of products you want with their quantities
+            Dictionary<string, int> desiredProducts = new Dictionary<string, int> { { "apple", 2 }, { "banana", 1 }, { "orange", 3 }, { "pear", 1 } };
 
-            // List of products you want
-            List<string> desiredProducts = new List<string> { "apple", "banana", "orange", "pear" }; // "pear" is not available in any store
-
-            (List<string> bestStoreNames, Dictionary<string, List<string>> productsToBuy, double totalPrice) = FindBestCombinationStores(stores, storeNames, desiredProducts, 2);
+            (List<string> bestStoreNames, Dictionary<string, Dictionary<string, int>> productsToBuy, double totalPrice) = FindBestCombinationStores(stores, storeNames, desiredProducts);
 
             Console.WriteLine("Best combination of stores:");
 
             string output = "";
-            
+
+            double grandTotal = 0;
+
             foreach (string storeName in bestStoreNames)
             {
                 output += "\n" + storeName;
-                output += "\n" + "Products to buy: " + string.Join(", ", productsToBuy[storeName]);
+                output += "\n" + "Products to buy: ";
+                double storeTotal = 0;
+                foreach (var product in productsToBuy[storeName])
+                {
+                    double productPrice = stores[storeNames.IndexOf(storeName)][product.Key];
+                    double productTotalPrice = product.Value * productPrice;
+                    storeTotal += productTotalPrice;
+                    output += $"{product.Key}: {product.Value} (Price per item: {productPrice:C2}), ";
+                }
+                output = output.TrimEnd(' ', ','); // Trim trailing comma and space
+                output += $"\nStore Total: {storeTotal:C2}";
+                grandTotal += storeTotal;
             }
 
-            return output;
+            output += $"\nGrand Total for the trip: {grandTotal:C2}";
 
+            return output;
         }
 
-        static (List<string>, Dictionary<string, List<string>>, double) FindBestCombinationStores(List<Dictionary<string, double>> stores, List<string> storeNames, List<string> desiredProducts, int storeLimit)
+        static (List<string>, Dictionary<string, Dictionary<string, int>>, double) FindBestCombinationStores(List<Dictionary<string, double>> stores, List<string> storeNames, Dictionary<string, int> desiredProducts)
         {
             List<string> bestStoreNames = new List<string>();
-            Dictionary<string, List<string>> productsToBuy = new Dictionary<string, List<string>>();
+            Dictionary<string, Dictionary<string, int>> productsToBuy = new Dictionary<string, Dictionary<string, int>>();
 
             // Generate combinations of stores
-            List<List<int>> combinations = GenerateCombinations(stores.Count, storeLimit);
+            List<List<int>> combinations = GenerateCombinations(stores.Count);
 
             double minPrice = double.MaxValue;
             double totalPrice = 0;
@@ -60,10 +132,10 @@ namespace Shopping_list_builder.Classes
             foreach (var combination in combinations)
             {
                 double currentTotalPrice = 0;
-                Dictionary<string, List<string>> tempProductsToBuy = new Dictionary<string, List<string>>();
+                Dictionary<string, Dictionary<string, int>> tempProductsToBuy = new Dictionary<string, Dictionary<string, int>>();
 
                 // Calculate the total price and products for the combination
-                foreach (string product in desiredProducts)
+                foreach (string product in desiredProducts.Keys)
                 {
                     double productPrice = double.MaxValue;
                     string storeName = null;
@@ -82,14 +154,18 @@ namespace Shopping_list_builder.Classes
                     if (storeName == null)
                         continue;
 
+                    // Calculate the quantity to buy
+                    int quantityToBuy = desiredProducts[product];
+
                     // Add the product to the list of products to buy from the corresponding store
                     if (!tempProductsToBuy.ContainsKey(storeName))
                     {
-                        tempProductsToBuy[storeName] = new List<string>();
+                        tempProductsToBuy[storeName] = new Dictionary<string, int>();
                     }
-                    tempProductsToBuy[storeName].Add(product);
+                    tempProductsToBuy[storeName][product] = quantityToBuy;
 
-                    currentTotalPrice += productPrice;
+                    // Calculate the total price for this product
+                    currentTotalPrice += quantityToBuy * productPrice;
                 }
 
                 // Update the minimum total price and best store names if a new minimum is found
@@ -97,7 +173,7 @@ namespace Shopping_list_builder.Classes
                 {
                     minPrice = currentTotalPrice;
                     bestStoreNames = combination.Select(index => storeNames[index]).ToList();
-                    productsToBuy = new Dictionary<string, List<string>>(tempProductsToBuy);
+                    productsToBuy = new Dictionary<string, Dictionary<string, int>>(tempProductsToBuy);
                     totalPrice = currentTotalPrice;
                 }
             }
@@ -105,8 +181,8 @@ namespace Shopping_list_builder.Classes
             return (bestStoreNames, productsToBuy, totalPrice);
         }
 
-        // Helper method to generate combinations of store indices with the specified limit
-        static List<List<int>> GenerateCombinations(int n, int k)
+        // Helper method to generate combinations of store indices
+        static List<List<int>> GenerateCombinations(int n)
         {
             var combinations = new List<List<int>>();
 
@@ -120,8 +196,6 @@ namespace Shopping_list_builder.Classes
 
             return combinations;
         }
-
-
 
 
 
